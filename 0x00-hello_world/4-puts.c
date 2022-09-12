aed131c2258d7f3261a810b2703e3b30ea15382e
Banjo Adebimpe <adijatbimpe@gmail.com>
2022-09-11 17:10:29 -0700

    dive into c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+
+/**
+ * main - Function starts
+ *
+ * Return: Function closes
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	ruturn (0);
+}
