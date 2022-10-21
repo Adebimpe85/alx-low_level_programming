c01f8ad6f8a7bfa6788a11ce0ab6a11c02e8ab86
Banjo Adebimpe <adijatbimpe@gmail.com>
2022-10-21 03:31:27 -0700

    Singly

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct list_s - singly linked list
@@ -6,13 +8,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for Holberton project
+ * for Holberton project
  */
 typedef struct list_s
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
