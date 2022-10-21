0f7a75b5da3c593bd207c63a0181421250f88aaa
Banjo Adebimpe <adijatbimpe@gmail.com>
2022-10-21 04:19:21 -0700

    singly

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,26 +1,22 @@
-/*
- * File: 1-list_len.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * @h: The linked list_t list.
- *
- * Return: The number of elements in h.
- */
+  * list_len - Show the number of elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of elements of a list
+  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		elements++;
 		h = h->next;
+		count++;
 	}
 
-	return (elements);
+	return (count);
 }
