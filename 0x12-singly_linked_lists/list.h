e2de9c87a713c133fc96d4d9ba9205b431e5780a
Banjo Adebimpe <adijatbimpe@gmail.com>
2022-10-21 03:22:26 -0700

    singly

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,14 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x11-singly_linked_lists directory.
- */
-
-#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,9 +10,9 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+        char *str;
+        unsigned int len;
+        struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -32,4 +21,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
